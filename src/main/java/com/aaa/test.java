f9cf8982a574512e5ee206ad9dd5b95639d64676
z <zhangyi20220823@163.com>
2022-10-11 10:39:42 +0800

    Initial commit

---
+++ src/main/java/com/aaa/test.java
@@ -0,0 +1,13 @@
+package com.aaa;
+
+public class test {
+    public static void main(String[] args) {
+        a();
+    }
+
+    public static void a() {
+        int i = 1;
+        int j = 2;
+        System.out.println(i + j);
+    }
+}
